01f335ef70d4a42f46487ce54348fe536f7ef6d9
kevinptx <kevin.peery@gmail.com>
2017-09-16 18:56:16 -0500

    button added to render users on the client side through index.html

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+
+
 function loadFlipCardUsers() {
   fetch('http://localhost:3000/users')
   .then(function(response) {
@@ -5,10 +7,21 @@ function loadFlipCardUsers() {
   })
   .then(function(json){
     console.log(json)
+    let users = json
+    var usersList = document.querySelector("#usersList")
+    for (var i = 0; i < users.length; i++) {
+      let li = document.createElement("li")
+      li.innerHTML = users[i].username
+      usersList.appendChild(li)
+    }
   })
   .catch(function(error){
     console.log('parsing failed', error)
   })
 }
 
-loadFlipCardUsers()
+var userButton = document.querySelector("#userButton")
+
+userButton.addEventListener('click', function(){
+  loadFlipCardUsers()
+})
